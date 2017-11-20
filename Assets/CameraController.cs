5cc04ef6b8a47ec51721e83f097df08971ecea59
Rafael Pivetta Balbuena <rafael.pivetta@gmail.com>
2017-11-20 21:35:39 -0200

    Initial commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void LateUpdate(){
+		Vector3 v = new Vector3 (transform.position.x, 0.0f, transform.position.z);
+		transform.position = v;
+
+		Debug.Log (transform.position.y);
+	}
+}
