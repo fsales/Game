ee4d9b28bc8623f25aa479e5f9af10ff151e1318
Rafael Pivetta Balbuena <rafael.pivetta@gmail.com>
2017-11-22 08:07:53 -0200

    Teste de Atualização

---
+++ Assets/Scripts/RotateController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, 0, -10);
+
+//		Vector3 scala = transform.localScale;
+//		scala.x = 1;
+//		transform.localScale = scala;
+//
+//		Vector3 pos = transform.position;
+//		pos.x -= 0.2f;
+//
+//		transform.position = pos;
+	}
+}
