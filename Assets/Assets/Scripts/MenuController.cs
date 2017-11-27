bfd664c8d83e3b089d49da8d2c42938bcda70d5a
Fábio de Oliveira Sales <fabio.oliveira.sales@gmail.com>
2017-11-27 21:33:23 -0200

    Criação da Scena Menu

---
+++ Assets/Assets/Scripts/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuController : MonoBehaviour {
+
+    public string scenaNewGame;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void NewGame() {
+        SceneManager.LoadScene(sceneName: scenaNewGame);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
